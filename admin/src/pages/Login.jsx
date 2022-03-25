b7b4b8514606772cb05f728bdf539be180090a39
shaevtchac <shaevtchac@gmail.com>
2022-03-25 07:39:58 +0100

    login error msg for non admin

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -5,6 +5,8 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Button, EditInput } from "../components/Reusables";
 import { login } from "../redux/apiCalls";
+import { reset } from "../redux/userRedux";
+import { getToken } from "../requestMethods";
 
 const Container = styled.div`
   display: flex;
@@ -15,28 +17,39 @@ const Container = styled.div`
   flex-direction: column;
   width: 100%;
 `;
+const ErrorMsg = styled.h3`
+  color: red;
+`;
 
 const Login = () => {
-  const user = useSelector((state) => state.user.currentUser);
+  const user = useSelector((state) => state.user);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [errorMsg, setErrorMsg] = useState("");
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    login(dispatch, { email, password });
-    navigate("/");
+  const handleClick = async (e) => {
+    // e.preventDefault();
+    await login(dispatch, { email, password });
   };
   useEffect(() => {
-    if (user) {
-      navigate("/");
+    if (user.currentUser?.isAdmin) {
+      setErrorMsg("");
+      window.location.reload(true);
+      navigate(-1);
+    } else {
+      if (user.currentUser !== null) {
+        setErrorMsg("Wymagane uprawnienia administratora");
+        dispatch(reset());
+      }
     }
-  }, [user, navigate]);
+  }, [user.currentUser, navigate, dispatch]);
 
   return (
     <Container>
       <h2>Panel administratora – logowanie</h2>
+      <ErrorMsg>{errorMsg}</ErrorMsg>
       <EditInput
         type="text"
         placeholder="nazwa użytkownika"
@@ -49,7 +62,9 @@ const Login = () => {
         placeholder="hasło"
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button onClick={handleClick}>Logowanie</Button>
+      <Button filled disabled={user.isFetching} onClick={handleClick}>
+        Logowanie
+      </Button>
     </Container>
   );
 };

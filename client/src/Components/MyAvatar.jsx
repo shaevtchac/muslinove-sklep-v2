7ea8dff66ea450266b7fc91ef23dcbe4196f7eff
shaevtchac <shaevtchac@gmail.com>
2022-04-07 08:14:23 +0200

    avatar component

---
+++ client/src/Components/MyAvatar.jsx
@@ -0,0 +1,43 @@
+import Avatar from "@mui/material/Avatar";
+
+const MyAvatar = ({ username, rest }) => {
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.substr(-2);
+    }
+    /* eslint-enable no-bitwise */
+    return color;
+  }
+
+  function stringAvatar(name) {
+    if (name) {
+      return {
+        children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+      };
+    } else return { children: "NN" };
+  }
+
+  return (
+    <Avatar
+      {...stringAvatar(username)}
+      sx={{
+        cursor: "pointer",
+        bgcolor: stringToColor(username),
+      }}
+      {...rest}
+    />
+  );
+};
+
+export default MyAvatar;
